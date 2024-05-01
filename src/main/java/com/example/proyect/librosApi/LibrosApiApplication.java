7d7da8630140620c144527496d7c8732953a26f0
MamaniMarioOrlando <46626380+MamaniMarioOrlando@users.noreply.github.com>
2024-05-01 20:48:55 -0300

    clase para generar la conexion a la api

--- src/main/java/com/example/proyect/librosApi/LibrosApiApplication.java
+++ src/main/java/com/example/proyect/librosApi/LibrosApiApplication.java
@@ -1,13 +1,23 @@
 package com.example.proyect.librosApi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import service.ConsumoAPI;
 
 @SpringBootApplication
-public class LibrosApiApplication {
+public class LibrosApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrosApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		var consumoApi = new ConsumoAPI();
+
+		var json = consumoApi.obtenerDatos("http://gutendex.com/books/");
+		System.out.println(json);
+	}
 }
